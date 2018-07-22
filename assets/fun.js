d8a90ec6917e9e7251b7b39e335e9bcce2a5681d
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-07-21 23:26:51 -0400

    udemy exercise going over v-bind, methods and data insertion

--- assets/fun.js
+++ assets/fun.js
@@ -1,9 +1,29 @@
 new Vue({
     el: "#app",
     data: {
-        title: "Hola el Mundo!"
+        title: "Hola el Mundo!",
+        link: "http://google.com"
     },
     methods: {
-
+        changeTitle: function(event) {
+            this.title = event.target.value;
+        },
+        omg: function() {
+            return "omg lolz", this.title;
+        }
     }
 });
+
+new Vue({
+    el: "#exercise",
+    data: {
+        name: "Cat",
+        age: 11,
+        src: "https://assets.rbl.ms/4166588/980x.jpg"
+    },
+    methods: {
+        rando: function() {
+            return Math.random();
+        }
+    }
+})
